e57357d7f15f1bd89e185af642a6be80934abccb
josh182014 <josh182014@gmail.com>
2019-05-28 15:02:22 -0600

    added comment functionality

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -16,16 +16,46 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const post = await Posts.insert(req.body);
-        res.status(201).json(post);
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({
+                errorMessage: 'Please provide title and contents for the post',
+            })
+        }
+        else {
+            const post = await Posts.insert(req.body);
+            res.status(201).json(post);
+        }
     } catch (error) {
         // log error to database
         console.log(error);
         res.status(500).json({
-        message: 'Error adding the post',
+        error: 'There was an error while saving the post to the database',
         });
     }
 });
+
+router.post('/:id/comments', async (req, res) => {
+    try {
+        if (req.body.text) {
+            const count = await Posts.findById(req.body.post_id)
+            if (count[0]) {
+                const comment = await Posts.insertComment(req.body);
+                res.status(201).json(comment);
+            }
+            else {
+                res.status(404).json({message: 'The post with the specified ID does not exist.'})
+            }
+        }
+        else {
+            res.status(400).json({errorMessage: 'Please provide text for the comment'})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        error: 'There was an error while sving the comment to the database.',
+        });
+    }
+    });
     
 
 
