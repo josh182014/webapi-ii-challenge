99a8bf3f5816a526b493c0f68c84723964c97933
josh182014 <josh182014@gmail.com>
2019-05-28 15:36:42 -0600

    added comment get functionality

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -60,7 +60,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const post = await Posts.findById(req.params.id); 
-        res.status(200).json(post);
+        if (post.length) {
+            res.status(200).json(post);
+        }
+        else {
+            res.status(404).json({ message: "The post with the specified ID does not exist"})
+        }
     } catch(error) {
         console.log(error)
         res.status(500).json({
@@ -69,5 +74,19 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
+        if (post.length) {
+            const comments = await Posts.findPostComments(req.params.id)
+            res.status(200).json(comments)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+
+    } catch(error) {
+        res.status(500).json({ error: "The comments information could not be retrieved."})
+    }
+});
 
 module.exports = router;
