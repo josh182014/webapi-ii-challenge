c3db35cd6245f02f1349f8969f844e17501775a3
josh182014 <josh182014@gmail.com>
2019-05-28 15:46:07 -0600

    added delete functionality

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -89,4 +89,20 @@ router.get('/:id/comments', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
+        if (post.length) {
+            const post = await Posts.remove(req.params.id)
+            res.status(200).json({ message: `Post deleted` })
+        }
+        else {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+    }
+    catch(error) {
+        res.status(500).json({ error: "The post could not be removed" })
+    }
+})
+
 module.exports = router;
